343add2f52b2df81d4d1684218d944d911a2edd0
suryaaji016 <suryaaji016@gmail.com>
2025-10-22 12:36:09 +0700

    cek winner

--- server/index.js
+++ server/index.js
@@ -113,6 +113,26 @@ io.on("connection", (socket) => {
   });
 });
 
+function checkWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const [a, b, c] of lines) {
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return { winner: board[a], line: [a, b, c] }; // 🟢 kirim posisi menang
+    }
+  }
+  return null;
+}
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () =>
   console.log(`✅ Server running at http://localhost:${PORT}`)
